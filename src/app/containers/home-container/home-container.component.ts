cfaeb752b0e03ea926a012417fb5b64d3cfdf4ef
spiros3p <spiros3p@gmail.com>
2025-01-03 10:29:39 +0100

    Auto-generated commit

--- src/app/containers/home-container/home-container.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-home-container",
-  imports: [],
-  templateUrl: "./home-container.component.html",
-  styleUrl: "./home-container.component.scss",
-})
-export class HomeContainerComponent {}
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  bitaxeConfigs: {
-    bitaxeUrls: ["192.168.0.102"],
-  },
-};
